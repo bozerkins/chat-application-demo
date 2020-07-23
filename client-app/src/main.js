8066dda2e98efabcba7ed654e8ebcbac9b0556ad
bozerkins <b.ozerkins@gmail.com>
2020-07-24 00:10:55 +0300

    finished minor refactoring

--- client-app/src/main.js
+++ client-app/src/main.js
@@ -85,142 +85,129 @@ class Socket
 
 class ChatRoom
 {
-    constructor()
+    constructor(user)
     {
-
+        this.user = user;
+        this.autoscroll = true;
     }
     
     render()
     {
-        let user = getUser();
-        $('#username').text(user['cognito:username']);
-        $('#usernameControlDisplay').text('@' + user['cognito:username']);
+        $('#username').text(this.user['cognito:username']);
+        $('#usernameControlDisplay').text('@' + this.user['cognito:username']);
         $('#logoutButton').click(() => {
             document.cookie = "id_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
             window.location.reload();
         });
         
-        createPicker();
+        const button = document.querySelector('#emoji-button');
+        const picker = new EmojiButton({
+            autoFocusSearch: false,
+            showSearch: false,
+            autoHide: false
+        });
+    
+        picker.on('emoji', emoji => {
+            const input = $('#chat-message-input');
+            input.val(input.val() + emoji);
+            input.focus();
+        });
+    
+        button.addEventListener('click', () => {
+        picker.togglePicker(button);
+        });
 
         $('.chat-message-container').scroll(function() {
             let atTheBottom = ($(this).prop("scrollHeight") - Math.floor($(this).scrollTop() + $(this).outerHeight())) === 0;
             if (atTheBottom === false) {
-                autoscroll = false;
+                this.autoscroll = false;
             } else {
-                autoscroll = true;
+                this.autoscroll = true;
             }
         });
     }
 
-    checkUserOrRedirect()
+    renderMessage(message)
     {
-        if (getUser() === null) {
-            window.location.href = 'http://localhost:8001/login';
+        const name = `${message.author} (${message.sub})`;
+        const isThisMe = message.sub ===  this.user.sub;
+
+        const messageColor = isThisMe ? 'alert-info' : 'alert-secondary';
+        const messageFloat = isThisMe ? 'float-right' : 'float-left';
+        const authorFloat = isThisMe ? 'chat-author-me' : 'chat-author-not-me';
+        const rendered = `
+            <div class="chat-message w-100 p-1 clearfix">
+                <span class="alert ${messageColor} ${messageFloat} m-0">
+                    ${message.message}
+                </span>
+                <span class="chat-message-author ${authorFloat}">${name}</span>
+            </div>
+        `;
+        $('.chat-message-container').first().append(rendered);
+        
+        // scroll to bottom
+        if (this.autoscroll === true) {
+            const container = $('.chat-message-container');
+            container.scrollTop(container.prop("scrollHeight"));
         }
+    }
+
+    startControls(socket)
+    {
+        const input = $('#chat-message-input');
+        input.focus();
+        input.keyup(function(e) {
+            if(e.keyCode == 13) // Enter
+            {
+                const message = input.val();
+                if (!message) {
+                    return;
+                }
+                socket.send(JSON.stringify({ message: message }));
+
+                input.val('');
+                // scroll to bottom
+                const container = $('.chat-message-container');
+                container.scrollTop(container.prop("scrollHeight"));
+            }
+        });
     }
 }
 
+(function() {
+    // check authentication
+    const user = getUser();
+    if (user === null) {
+        window.location.href = 'http://localhost:8001/login';
+        return;
+    }    
 
-let autoscroll = true;
-let chatRoom = new ChatRoom();
-chatRoom.checkUserOrRedirect();
-
-let overlay = new Overlay();
-let socket = new Socket("ws://localhost:8000/");
-socket.bindOpen((event) => {
-    overlay.hide();
-    chatRoom.render();
-});
-socket.bindError((event) => {
-    overlay.error();
-    console.log(event);
-});
-socket.bindMessage((event) => {
-    const message = JSON.parse(event.data);
-    message.me = message.sub ===  getUser().sub;
-
-    renderMessage(`${message.author} (${message.sub})`, message.me, message.message)
-
-    // scroll to bottom
-    if (autoscroll === true) {
-        const container = $('.chat-message-container');
-        container.scrollTop(container.prop("scrollHeight"));
-    }
-});
-
-overlay.bindRetry((event) => {
+    //create the chat room
+    let chatRoom = new ChatRoom(user);
+    let overlay = new Overlay();
+    let socket = new Socket("ws://localhost:8000/");
+    socket.bindOpen((event) => {
+        overlay.hide();
+        chatRoom.render();
+        chatRoom.startControls(socket);
+    });
+    socket.bindError((event) => {
+        overlay.error();
+        console.log(event);
+    });
+    socket.bindMessage((event) => {
+        const message = JSON.parse(event.data);
+        chatRoom.renderMessage(message);
+    });
+    
+    overlay.bindRetry((event) => {
+        overlay.loading();
+        socket.connect();
+    });
+    overlay.bindClose((event) => {
+        window.location.href = 'index.html';
+    });
+    
     overlay.loading();
     socket.connect();
-});
-overlay.bindClose((event) => {
-    window.location.href = 'index.html';
-});
-
-overlay.loading();
-socket.connect();
-
-initControls(socket);
-
-function renderMessage(name, isThisMe, message)
-{
-    const messageColor = isThisMe ? 'alert-info' : 'alert-secondary';
-    const messageFloat = isThisMe ? 'float-right' : 'float-left';
-    const authorFloat = isThisMe ? 'chat-author-me' : 'chat-author-not-me';
-    const rendered = `
-        <div class="chat-message w-100 p-1 clearfix">
-            <span class="alert ${messageColor} ${messageFloat} m-0">
-                ${message}
-            </span>
-            <span class="chat-message-author ${authorFloat}">${name}</span>
-        </div>
-    `;
-    $('.chat-message-container').first().append(rendered);
-}
-
-function inputEnterEvent(input, server)
-{
-    const message = input.val();
-    if (!message) {
-        return;
-    }
-    server.send(JSON.stringify(
-        { message: message }
-    ));
-
-    input.val('');
-    // scroll to bottom
-    const container = $('.chat-message-container');
-    container.scrollTop(container.prop("scrollHeight"));
-}
-
-function initControls(server)
-{
-    const input = $('#chat-message-input');
-    input.focus();
-    input.keyup(function(e) {
-        if(e.keyCode == 13) // Enter
-        {
-            inputEnterEvent(input, server);
-        }
-    });
-}
-
-function createPicker()
-{
-    const button = document.querySelector('#emoji-button');
-    const picker = new EmojiButton({
-        autoFocusSearch: false,
-        showSearch: false,
-        autoHide: false
-    });
-   
-    picker.on('emoji', emoji => {
-        const input = $('#chat-message-input');
-        input.val(input.val() + emoji);
-        input.focus();
-    });
-   
-    button.addEventListener('click', () => {
-      picker.togglePicker(button);
-    });
-}
+})();
